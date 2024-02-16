e0bb139a1077021c03a29fa55a6a9385aca199f3
rayssabuarque <brayssa17@gmail.com>
2024-02-16 12:03:00 -0300

    start off

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Website Cedir 🖥️💾💻 (backend) 
 
+!(Diagrama de estruturação do CRUD)[diagram.jpeg]
+
 Desenvolvido por [Klayvem Guimarães](https://github.com/KlayvemGuimaraes) e [Rayssa Buarque](https://github.com/RayssaBuarque)
 
 ## NOTAS:
--- src/crud-routes/user/create.js
+++ src/crud-routes/user/create.js
@@ -1,3 +1,25 @@
 const express = require("express");
 
 const create = express.Router();
+const {PrismaClient} = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+/*
+    Create route for user table
+*/
+create.post('/user', async (req, res) => {
+    const {nome, cpf, email} = req.body; 
+
+    const user = await prisma.user.create({
+        data:{
+            nome,
+            cpf,
+            email,
+        },
+    });
+    return res.status(201).json(user);
+});
+
+
+module.exports = create;
