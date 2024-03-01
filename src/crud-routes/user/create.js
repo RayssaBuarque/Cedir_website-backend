2f2960c28a2962cc5316c064b0c836151bb8fe04
rayssabuarque <brayssa17@gmail.com>
2024-03-01 18:44:08 -0300

    cpf validation

--- src/crud-routes/user/create.js
+++ src/crud-routes/user/create.js
@@ -22,6 +22,65 @@ async function checarExistencia(cpfUser, emailUser){
         return existencia;
     }catch(error){console.log(error)}
 }
+async function checarCpf(cpfUser){
+    const validaPrimeiroDigito = (cpf) => { // Calcula e verifica se o primeiro dígito verificador do CPF é válido.
+        let soma = 0;
+        let multiplicador = 10;
+    
+        for (let tamanho = 0; tamanho < 9; tamanho++) {
+          soma += parseInt(cpf[tamanho]) * multiplicador;
+          multiplicador--;
+        }
+    
+        soma = (soma * 10) % 11;
+    
+        if (soma === 10 || soma === 11) {
+          soma = 0;
+        }
+    
+        return soma !== parseInt(cpf[9]);
+      };
+    const validaSegundoDigito = (cpf) => { // Calcula e verifica se o segundo dígito verificador do CPF é válido.
+        let soma = 0;
+        let multiplicador = 11;
+    
+        for (let tamanho = 0; tamanho < 10; tamanho++) {
+          soma += parseInt(cpf[tamanho]) * multiplicador;
+          multiplicador--;
+        }
+    
+        soma = (soma * 10) % 11;
+    
+        if (soma === 10 || soma === 11) {
+          soma = 0;
+        }
+    
+        return soma !== parseInt(cpf[10]);
+      };
+    const validaNumerosRepetidos = (cpf) => {
+        const numerosRepetidos = [ // Verifica se o cpf tem uma sequencia de n repetidos
+          '00000000000',
+          '11111111111',
+          '22222222222',
+          '33333333333',
+          '44444444444',
+          '55555555555',
+          '66666666666',
+          '77777777777',
+          '88888888888',
+          '99999999999'
+        ];
+    
+        return numerosRepetidos.includes(cpf); // Retorna true se repetido, false caso contrário.
+        //includes = verifica se em (cpf), há algum cpf presente em numerosRepetidos 
+      };
+
+      if (validaNumerosRepetidos(cpfUser) || validaPrimeiroDigito(cpfUser) || validaSegundoDigito(cpfUser)) { // || =  retorna verdadeiro caso ambos os operandos sejam verdadeiro, e se for falso, retorna falso.
+        return true
+      } else {
+        return false
+      }
+}
 async function createUserPJ(nomeUser, nomeEmpresa, cpfUser, cnpjEmpresa, emailUser) {
     try{ 
         const checagem = await checarExistencia(cpfUser, emailUser);
@@ -50,14 +109,15 @@ async function createUserPJ(nomeUser, nomeEmpresa, cpfUser, cnpjEmpresa, emailUs
 async function createUserPF(nomeUser, cpfUser, emailUser){
     try{
         const campos = [nomeUser, cpfUser, emailUser]
-        const checagem = await checarExistencia(cpfUser, emailUser);
+        const checagemUsuario = await checarExistencia(cpfUser, emailUser);
+        const checagemCpf = await checarCpf(cpfUser);
 
         if(campos.some(item => item === "")){
             return({mensagem: "Preencha todos os campos"})
-        }else if(!!checagem){
+        }else if(!!checagemUsuario){
             return({mensagem: "Usuário já cadastrado"})
-        // }else if(){ 
-            // CONDIÇÃO DA API DO CPF
+        }else if(!!checagemCpf){ 
+            return({mensagem: "CPF inválido"})
         }else{
             const user = await prisma.user.create({
                 data:{
